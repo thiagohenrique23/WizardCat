2e6d6d0d18f42490ddc52ff1a8fb5df6c0c9d054
thiagohenrique23 <thiagohenrique2309@gmail.com>
2023-04-25 17:59:34 -0400

    Parte de login sem funcionabilidade

--- wizardCat/App.js
+++ wizardCat/App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import { StatusBar } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>XD</Text>
-      <StatusBar style="auto" />
-    </View>
+  return(
+    <NavigationContainer>
+      <StatusBar backgroundColor='#000' barStyle='light-content' />
+      <Routes/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- wizardCat/src/pages/Welcome/index.js
+++ wizardCat/src/pages/Welcome/index.js
@@ -1,10 +1,81 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import * as Animatable from 'react-native-animatable'
+import { useNavigation } from "@react-navigation/native";
 
 export default function Welcome() {
+    
+    const navigation = useNavigation();
+    
     return (
-        <View>
-            <Text>Bem vindo</Text>
+        
+        <View style={styles.container}>
+            <View style={styles.containerLogo}>
+                <Animatable.Image
+                    animation='flipInY'
+                    source={require('../../assets/logo.png')}
+                    style={{ width: 300, height: 300 }}
+                />
+            </View>
+
+            <Animatable.View delay={600} animation='fadeInUp' style={styles.containerForm}>
+                <Text style={styles.title}>Seja bem-vindo ao meu reino felino, humano, prepare-se para uma jornada mágica.</Text>
+                <Text style={styles.text}>Faça o login para começar</Text>
+
+                <TouchableOpacity style={styles.button} onPress={ () => navigation.navigate('SignUp')}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </Animatable.View>
+
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000'
+    },
+    containerLogo: {
+        flex: 2,
+        backgroundColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        borderTopRightRadius: 25,
+        borderTopLeftRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 28,
+        marginBottom: 12,
+        color: '#8B008B'
+    },
+    text: {
+        color: '#8A2BE2'
+    },
+    button: {
+        position: 'absolute',
+        backgroundColor: '#8A2BE2',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: '15%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText:{
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold'
+    }
+})
